393e46ae861eb88c67fe3721037001a6c69e6aa7
marcusasker <55256718+marcusasker@users.noreply.github.com>
2021-05-06 09:11:37 +0200

    as

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -12,7 +12,6 @@ test('peek on stack with one element returns that element', () => {
 
 test('peek on stack with two or more elements returns the top element', () => {
     stack.push(1);
-    stack.push("wow");
     stack.push(42);
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
